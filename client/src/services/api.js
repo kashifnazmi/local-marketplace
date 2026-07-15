46df9dfb9367981bb0881e24cfb4ea7cd99fcc3a
kashifnazmi <12345678+kashifnazmi@users.noreply.github.com>
2026-07-15 16:52:38 +0530

    Configure frontend for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
 });
 
-api.interceptors.request.use((config) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-  if (user?.token) {
-    config.headers.Authorization = `Bearer ${user.token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
